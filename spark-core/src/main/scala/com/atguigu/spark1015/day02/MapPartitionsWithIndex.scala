37a2496f60ddba569962c5a34b93341c817c2de6
John Doe <30752740@qq.com>
2020-03-13 13:47:29 +0800

    更新

--- spark-core/src/main/scala/com/atguigu/spark1015/day02/MapPartitionsWithIndex.scala
+++ spark-core/src/main/scala/com/atguigu/spark1015/day02/MapPartitionsWithIndex.scala
@@ -8,7 +8,7 @@ object MapPartitionsWithIndex {
         val conf: SparkConf = new SparkConf().setAppName("MapPartitionsWithIndex").setMaster("local[2]")
         val sc: SparkContext = new SparkContext(conf)
         val list1 = List(30, 50, 70, 60, 10, 20)
-        val rdd1: RDD[Int] = sc.parallelize(list1, 4)
+        val rdd1: RDD[Int] = sc.parallelize(list1)
         // (30, 0), (50, 0)...
         val rdd2 = rdd1.mapPartitionsWithIndex((index, it) => {
             it.map(x => (index, x))
