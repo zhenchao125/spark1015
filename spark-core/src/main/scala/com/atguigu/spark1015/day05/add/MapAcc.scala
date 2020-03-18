6c1f7fe0c3d5fe9416e255553beb4faf04467eeb
John Doe <30752740@qq.com>
2020-03-18 08:17:19 +0800

    更新

--- spark-core/src/main/scala/com/atguigu/spark1015/day05/add/MapAcc.scala
+++ spark-core/src/main/scala/com/atguigu/spark1015/day05/add/MapAcc.scala
@@ -1,14 +1,14 @@
 package com.atguigu.spark1015.day05.add
 
 import org.apache.spark.rdd.RDD
-import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.util.AccumulatorV2
+import org.apache.spark.{SparkConf, SparkContext}
 
-object MyAcc{
+object MyAcc {
     def main(args: Array[String]): Unit = {
-        val conf: SparkConf = new SparkConf().setAppName("MyAcc").setMaster("local[2]")
+        val conf: SparkConf = new SparkConf().setAppName("MyAcc").setMaster("local[4]")
         val sc: SparkContext = new SparkContext(conf)
-        val list1 = List(30, 50, 70, 60, 10, 20)
+        val list1 = List(30, 50, 70, 60, 10, 20,10,30,40,50)
         val rdd1: RDD[Int] = sc.parallelize(list1, 2)
         
         val acc = new MapAcc
@@ -38,7 +38,10 @@ class MapAcc extends AccumulatorV2[Double, Map[String, Any]] {
     }
     
     // 不可变集合, 直接赋值一个空的集合
-    override def reset(): Unit = map = Map[String, Any]()
+    override def reset(): Unit = map = {
+        println("reset...")
+        Map[String, Any]()
+    }
     
     override def add(v: Double): Unit = {
         // 对sum和count进行累加. avg在最后value函数进行计算
