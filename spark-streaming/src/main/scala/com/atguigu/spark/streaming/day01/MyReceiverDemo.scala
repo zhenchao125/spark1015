065d721546b19c125f53b80bf42bea31a3707a08
John Doe <30752740@qq.com>
2020-03-23 11:01:45 +0800

    更新

--- spark-streaming/src/main/scala/com/atguigu/spark/streaming/day01/MyReceiverDemo.scala
+++ spark-streaming/src/main/scala/com/atguigu/spark/streaming/day01/MyReceiverDemo.scala
@@ -1,5 +1,8 @@
 package com.atguigu.spark.streaming.day01
 
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.streaming.receiver.Receiver
+
 /**
  * Author atguigu
  * Date 2020/3/23 10:41
@@ -8,4 +11,10 @@ object MyReceiverDemo {
     def main(args: Array[String]): Unit = {
         
     }
+}
+// 接收器从socket接受数据
+class MyReceiver extends Receiver(StorageLevel.MEMORY_ONLY) {
+    override def onStart(): Unit = ???
+    
+    override def onStop(): Unit = ???
 }
