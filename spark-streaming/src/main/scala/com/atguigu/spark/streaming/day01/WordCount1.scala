84ae5fb47be8c48e2b5d853dfa78363939853c6b
lzc <30752740@qq.com>
2020-04-14 14:35:00 +0800

    更新

--- spark-streaming/src/main/scala/com/atguigu/spark/streaming/day01/WordCount1.scala
+++ spark-streaming/src/main/scala/com/atguigu/spark/streaming/day01/WordCount1.scala
@@ -1,5 +1,8 @@
 package com.atguigu.spark.streaming.day01
 
+
+import org.apache.hadoop.io.{IntWritable, Text}
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
@@ -9,16 +12,22 @@ import org.apache.spark.streaming.{Seconds, StreamingContext}
  */
 object WordCount1 {
     def main(args: Array[String]): Unit = {
+        System.setProperty("HADOOP_USER_NAME", "atguigu")
         // 1. 创建StreamingContext
         val conf = new SparkConf().setMaster("local[2]").setAppName("WordCount1")
         val ssc = new StreamingContext(conf, Seconds(3))
         // 2. 从数据源创建一个流:  socket, rdd队列, 自定义接收器,    kafka(重点)
         val sourceStream = ssc.socketTextStream("hadoop102", 9999)
         // 3. 对流做各种转换
-//        val result = sourceStream.flatMap(_.split(" ")).map((_, 1)).reduceByKey(_ + _)
-        val result = sourceStream.flatMap(x => {Thread.sleep(10000); x.split(" ")}).map((_, 1)).reduceByKey(_ + _)
+        val result = sourceStream.flatMap(_.split(" ")).map((_, 1)).reduceByKey(_ + _)
+        //        val result = sourceStream.flatMap(x => {Thread.sleep(10000); x.split(" ")}).map((_, 1)).reduceByKey(_ + _)
         // 4. 行动算子 print  foreach foreachRDD
-        result.print()  // 把结果打印在控制台
+//        result.print() // 把结果打印在控制台
+        result.saveAsNewAPIHadoopFiles("/1015/test",
+            "wc",
+            classOf[Text],
+            classOf[IntWritable],
+            classOf[TextOutputFormat[Text, IntWritable]])
         // 5. 启动流
         ssc.start()
         // 6. 阻止主线程退出(阻塞主线程)
